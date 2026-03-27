ba3deda4ea7c7c8e5f4f89c1a3863120add1ba31
guts-718 <rahul.20223195@mnnit.ac.in>
2026-03-27 09:44:27 +0530

    removed vulnerability

--- get_chat_id.py
+++ get_chat_id.py
@@ -1,6 +1,6 @@
 import requests
 
-TOKEN = "8656007370:AAE5SINe0dLpcjoHChNH0nJdk8MsdW3iHI8"
+TOKEN= "secret"
 
 url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
 print(requests.get(url).json())
